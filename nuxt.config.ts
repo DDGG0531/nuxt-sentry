0d6e1e2d1d10ab35a73f21163759d2b056d178c7
Jim <z887700101703027@gmail.com>
2024-12-10 12:08:15 +0800

    fix: add sentry authToken

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ['@sentry/nuxt/module'],
+  modules: ["@sentry/nuxt/module"],
 
   sentry: {
     sourceMapsUploadOptions: {
-      org: 'ddgg',
-      project: 'javascript-nuxt'
-    }
+      org: "ddgg",
+      project: "javascript-nuxt",
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+    },
   },
 
   sourcemap: {
-    client: 'hidden'
-  }
-})
+    client: "hidden",
+  },
+});
